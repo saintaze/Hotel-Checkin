06891391ae5641be89afe1bda7aaac9f08d7e5ac
Ayaz Ahmed <ayez_ahmed@hotmail.com>
2019-08-31 04:42:27 +0500

    created room file

---
+++ room.rb
@@ -0,0 +1,25 @@
+class Room
+  attr_reader :occupants  
+
+  def initialize(capacity = rand(2..5))
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def full?
+    @capacity == @occupants.size
+  end
+
+  def available_space?
+    @capacity - @occupants.size
+  end
+
+  def add_occupant(name)
+    if available_space? > 0
+      @occupants << name
+      true
+    else
+      false
+    end
+  end
+end
